2cb5753421b01f0a13ed3446759350d7ac0af90b
Frank Wang <ftwang@gmail.com>
2019-12-04 13:47:56 +0800

    Hack to pass error from stderr stream to output
    - Pass stderr to output stream

---
+++ spec/error.spec.js
@@ -0,0 +1,37 @@
+var chai = require('chai'),
+    expect = chai.expect,
+    Ledger = require('../lib/ledger').Ledger;
+
+describe('Error', function() {
+  var spec;
+  var ledgerBinary = null;
+  
+  beforeEach(function() {
+    spec = this;
+    ledgerBinary = process.env.LEDGER_BIN || '/usr/local/bin/ledger';
+  });
+  
+  describe('display parsing error from ledger', function() {
+    var ledger;
+    
+    beforeEach(function(done) {
+      ledger = new Ledger({
+        file: 'spec/data/non-existant.dat',
+        binary: ledgerBinary,
+        debug: false
+      });
+      done();
+    });
+
+    it('should throw error stating file does not exist', function (done) {
+      ledger.balance()
+        .once('end', function(){
+          done();
+        })
+        .on('error', function(error) {
+          expect(error).to.match(/^Error: Cannot read journal file/);
+          done();
+        });
+    });
+  });
+});
